9ce6f7f68e49c486805996e201f00eded1b96e8e
Willa Waliyadin <59253075+lfcyk@users.noreply.github.com>
2023-08-10 02:00:20 +0700

    add basic html css js structure

---
+++ script.js
@@ -0,0 +1,14 @@
+const Gameboard = () => {
+    let board = [3][3];
+    const getBoard = () => board;
+    const setBoard = (player) => {
+        
+    }
+
+
+    return {getBoard};
+}
+
+let udin = Gameboard();
+
+console.log(udin.getBoard());
